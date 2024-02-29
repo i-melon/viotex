8f73f5433de0bba82b14dafc13d06244a36ee595
Melon West <i.melonpub@gmail.com>
2024-02-29 13:53:34 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        'lgreen':'rgba(209, 219, 189, 1)',
+        'lgreen2':'rgba(110, 133, 121, 1)',
+        'bgcolor':'rgba(25, 52, 65, 1)'
+      }
+    },
+  },
+  plugins: [],
+}
+
