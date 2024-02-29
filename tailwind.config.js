685a142c7010d37e9ba1c2cdad8eef9f0d8f93da
Askeev <askeev9601@gmail.com>
2024-02-29 18:02:14 +0600

    First_commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
       colors:{
         'lgreen':'rgba(209, 219, 189, 1)',
         'lgreen2':'rgba(110, 133, 121, 1)',
-        'bgcolor':'rgba(25, 52, 65, 1)'
+        'bgcolor':'rgba(25, 52, 65, 1)',
+        'button-color':'rgba(39, 47, 48, 0.80);'
       }
     },
   },
